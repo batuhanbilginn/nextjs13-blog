d34d2ee717b6dd236d52f7d1826e7b2bfc5e8239
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-24 14:45:23 +0300

    episode-4

---
+++ app/about/company/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Company Page</h1>
+    </div>
+  );
+};
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Hello world!</main>;
+  return (
+    <main>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </main>
+  );
 }
