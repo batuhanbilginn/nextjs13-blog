ca0f1720e12eb529d7ae416b82078277ce52ef14
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-24 21:58:39 +0300

    episode-22

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,3 +1,13 @@
+import { DUMMY_CATEGORIES, DUMMY_POSTS } from "@/DUMMY_DATA";
+
+export const generateStaticParams = async () => {
+  return DUMMY_CATEGORIES.map((category) => {
+    return {
+      category: category.slug,
+    };
+  });
+};
+
 const Page = ({
   params,
 }: {
@@ -5,7 +15,10 @@ const Page = ({
     category: string;
   };
 }) => {
-  return <div>{params.category}</div>;
+  const posts = DUMMY_POSTS.filter(
+    (post) => post.category.title.toLocaleLowerCase() === params.category
+  );
+  return <div>{JSON.stringify(posts)}</div>;
 };
 
 export default Page;
