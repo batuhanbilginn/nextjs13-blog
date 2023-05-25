d5dd34242fe686a720572021deb46da87f91963e
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-25 23:05:28 +0300

    episode-57

--- components/post/post-body.tsx
+++ components/post/post-body.tsx
@@ -1,8 +1,28 @@
-import parse from "html-react-parser";
+import parse, { Element } from "html-react-parser";
+import Image from "next/image";
 
 const PostBody = ({ body }: { body: string }) => {
+  const options = {
+    replace: (domNode: any) => {
+      if (domNode instanceof Element && domNode.attribs) {
+        if (domNode.name === "img") {
+          const { src, alt } = domNode.attribs;
+          return (
+            <Image
+              className="object-cover object-center w-full my-3 rounded-md h-auto max-h-[300px] md:max-h-[500px]"
+              src={src}
+              alt={alt}
+              width={1280}
+              height={620}
+            />
+          );
+        }
+      }
+    },
+  };
+
   const getParsedHTML = (body: string) => {
-    return parse(body);
+    return parse(body, options);
   };
 
   return <div className="rich-text">{getParsedHTML(body)}</div>;
