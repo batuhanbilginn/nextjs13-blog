8199b9d391f86309493c1365f542808e739c4ab9
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-25 22:59:09 +0300

    episode-55

--- components/post/post-body.tsx
+++ components/post/post-body.tsx
@@ -1,5 +1,11 @@
+import parse from "html-react-parser";
+
 const PostBody = ({ body }: { body: string }) => {
-  return <div>{body}</div>;
+  const getParsedHTML = (body: string) => {
+    return parse(body);
+  };
+
+  return <div>{getParsedHTML(body)}</div>;
 };
 
 export default PostBody;
