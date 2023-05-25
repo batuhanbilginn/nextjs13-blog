f3f61b8910a53a1cce37533cc304b192d0ac25ba
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-25 18:13:41 +0300

    episode-48

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,49 @@ import CTACard from "@/components/elements/cta-card";
 import PaddingContainer from "@/components/layout/padding-container";
 import PostCard from "@/components/post/post-card";
 import PostList from "@/components/post/post-lists";
-import { DUMMY_POSTS } from "@/DUMMY_DATA";
+import directus from "@/lib/directus";
+import { notFound } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const getAllPosts = async () => {
+    try {
+      const posts = await directus.items("post").readByQuery({
+        fields: [
+          "*",
+          "author.id",
+          "author.first_name",
+          "auhtor.last_name",
+          "category.id",
+          "category.title",
+        ],
+      });
+
+      return posts.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error fetching posts");
+    }
+  };
+
+  const posts = await getAllPosts();
+
+  console.log(posts);
+
+  if (!posts) {
+    notFound();
+  }
+
   return (
     <PaddingContainer>
       <main className="space-y-10">
-        <PostCard post={DUMMY_POSTS[0]} />
+        <PostCard post={posts[0]} />
         <PostList
-          posts={DUMMY_POSTS.filter((_post, index) => index > 0 && index < 3)}
+          posts={posts.filter((_post, index) => index > 0 && index < 3)}
         />
         <CTACard />
-        <PostCard reverse post={DUMMY_POSTS[3]} />
+        <PostCard reverse post={posts[3]} />
         <PostList
-          posts={DUMMY_POSTS.filter((_post, index) => index > 3 && index < 6)}
+          posts={posts.filter((_post, index) => index > 3 && index < 6)}
         />
       </main>
     </PaddingContainer>
