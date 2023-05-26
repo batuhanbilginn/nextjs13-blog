d8a31eb97321116ecae6e4a67dd6bb8db4601d6f
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-26 13:10:27 +0300

    episode-59

--- components/elements/cta-card.tsx
+++ components/elements/cta-card.tsx
@@ -1,4 +1,5 @@
 import directus from "@/lib/directus";
+import { revalidateTag } from "next/cache";
 import Image from "next/image";
 
 /* eslint-disable react/no-unescaped-entities */
@@ -10,10 +11,23 @@ const CTACard = async () => {
       await directus.items("subscribers").createOne({
         email,
       });
+      revalidateTag("subscribers-count");
     } catch (error) {
       console.log(error);
     }
   };
+
+  const subscribersCount = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}items/subscribers?meta=total_count&access_token=${process.env.ADMIN_TOKEN}`,
+    {
+      next: {
+        tags: ["subscribers-count"],
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res.meta.total_count)
+    .catch((error) => console.log(error));
 
   return (
     <div className="relative px-6 py-10 overflow-hidden rounded-md bg-slate-100">
@@ -39,19 +53,29 @@ const CTACard = async () => {
         </p>
         {/* Form */}
         <form
+          key={subscribersCount + "subscribers-form"}
           action={formAction}
           className="flex items-center w-full gap-2 mt-6"
         >
           <input
             type="email"
             name="email"
-            placeholder="Write your email. "
+            placeholder="Write your email."
             className="w-full px-3 py-2 text-base rounded-md outline-none md:w-auto placeholder:text-sm bg-white/80 focus:ring-2 ring-neutral-600"
           />
           <button className="px-3 py-2 rounded-md whitespace-nowrap bg-neutral-900 text-neutral-200">
             Sign Up
           </button>
         </form>
+
+        {/* Subscribers */}
+        <div className="mt-5 text-neutral-700">
+          Join our{" "}
+          <span className="px-2 py-1 text-sm rounded-md bg-neutral-700 text-neutral-100">
+            {subscribersCount}
+          </span>{" "}
+          subscribers now!
+        </div>
       </div>
     </div>
   );
