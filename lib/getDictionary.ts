07ddba8c80ef8ffa5537613dd28c7a57c925c195
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-26 16:17:33 +0300

    episode-66

---
+++ lib/getDictionary.ts
@@ -0,0 +1,14 @@
+const dictionaries = {
+  en: () =>
+    import("./../dictinoaries/en.json").then((module) => module.default),
+  de: () =>
+    import("./../dictinoaries/de.json").then((module) => module.default),
+};
+
+const getDictionary = async (locale: string) => {
+  if (!locale || locale === undefined) {
+    return dictionaries["en"];
+  } else {
+    return dictionaries[locale as "en" | "de"];
+  }
+};
