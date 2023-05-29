fd619ba1fec73652a5ba9bf84f6997af83abbad5
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-29 16:00:52 +0300

    episode-77

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,15 +1,29 @@
 import Footer from "@/components/navigation/footer";
 import Navigation from "@/components/navigation/navigation";
-import { Metadata } from "next";
+import { getDictionary } from "@/lib/getDictionary";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: "Explorer",
   description:
     "A minimal and lovely travel blog which shares experiences and citiest around the world!",
+}; */
+
+export const generateMetadata = async ({
+  params: { lang },
+}: {
+  params: { lang: string };
+}) => {
+  // Get the Dicitionary based on Lang
+  const dicitionary = await getDictionary(lang);
+
+  return {
+    title: "Explorer",
+    description: dicitionary.footer.description,
+  };
 };
 
 export default function RootLayout({
