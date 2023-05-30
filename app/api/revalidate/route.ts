665ae13c781a3f7a0b02e5fe6bdcb5a4ac2146d4
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-30 18:39:18 +0300

    revalidate

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,17 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const slug = request.nextUrl.searchParams.get("slug");
+  const categorySlug = request.nextUrl.searchParams.get("category-slug");
+  // Revalidate the Post
+  revalidatePath(`/en/post/${slug}`);
+  revalidatePath(`/de/post/${slug}`);
+  // Revalidate the Category
+  revalidatePath(`/en/${categorySlug}`);
+  revalidatePath(`/de/${categorySlug}`);
+  // Revalidate Home
+  revalidatePath(`/en`);
+  revalidatePath(`/de`);
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
