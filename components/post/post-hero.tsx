584acc6c100af05cb64b21da173d6b4e7060d87c
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-30 16:12:36 +0300

    post hero image priority

--- components/post/post-hero.tsx
+++ components/post/post-hero.tsx
@@ -12,6 +12,7 @@ const PostHero = ({ post, locale }: PostHeroProps) => {
       {/* @ts-expect-error Async Server Component */}
       <PostContent locale={locale} isPostPage post={post} />
       <Image
+        priority
         className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6"
         src={`${process.env.NEXT_PUBLIC_ASSETS_URL}${post.image}?key=optimised`}
         width={1280}
