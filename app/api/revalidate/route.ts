a912e6cb057ebe699af612c2c63f23d6b36bf4fe
batuhanbilginn <batuhanbilginn@gmail.com>
2023-05-31 13:19:30 +0300

    revalidate api

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,17 @@
-import { NextRequest } from "next/server";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, res: any) {
-  const path = request.nextUrl.searchParams.get("path");
-
-  return await res.revalidate(`${path}`);
+export async function GET(request: NextRequest) {
+  const slug = request.nextUrl.searchParams.get("slug");
+  const categorySlug = request.nextUrl.searchParams.get("category-slug");
+  // Revalidate the Post in Every Language
+  revalidatePath(`/en/post/[slug]`);
+  revalidatePath(`/de/post/[slug]`);
+  // Revalidate the Category
+  revalidatePath(`/en/[category]`);
+  revalidatePath(`/de/[category]`);
+  // Revalidate Home
+  revalidatePath(`/en`);
+  revalidatePath(`/de`);
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
